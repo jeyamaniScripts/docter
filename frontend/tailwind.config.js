668ccafca02a0c5c4e8382af4794e170895500b7
jeyamaniScripts <thejeyamani034@gmail.com>
2025-01-02 22:15:00 +0530

    add tailwind css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#5f6fff",
+      },
+      gridTemplateColumns: {
+        auto: "repeat(auto-fill ,minmax(200px,1fr))",
+      },
+    },
   },
   plugins: [],
 };
